6c0ab780984b944bb83f7e36894eb979a9205170
Zak Gogi <zakgogi1@gmail.com>
2021-07-20 13:01:37 +0100

    added functionality to allow user to check where they went wrong, and better messaging for correct solution

--- index.mjs
+++ index.mjs
@@ -11,12 +11,24 @@ const mediumBtn = document.getElementById('mediumButton');
 const hardBtn = document.getElementById('hardButton');
 easyBtn.addEventListener('click', () => {
     newSudoku('Easy');
+    let submitSection = document.getElementById('submitSection');
+    submitSection.style.display = "block";
+    let solutionSection = document.getElementById('solutionSection');
+    solutionSection.innerHTML = "";
 })
 mediumBtn.addEventListener('click', () => {
     newSudoku('Medium');
+    let submitSection = document.getElementById('submitSection');
+    submitSection.style.display = "block";
+    let solutionSection = document.getElementById('solutionSection');
+    solutionSection.innerHTML = "";
 })
 hardBtn.addEventListener('click', () => {
     newSudoku('Hard');
+    let submitSection = document.getElementById('submitSection');
+    submitSection.style.display = "block";
+    let solutionSection = document.getElementById('solutionSection');
+    solutionSection.innerHTML = "";
 })
 
 function newSudoku(difficulty){
@@ -34,6 +46,7 @@ function newSudoku(difficulty){
             if (dokuToTranslate[i][j] === 0){
                 let inputField = document.createElement('input');
                 inputField.type = "text";
+                inputField.id = `${i}${j}`;
                 if (j%3 === 0){
                     inputField.classList.add('boldCol');
                 } else if (j%8 === 0){
@@ -76,9 +89,11 @@ function compareSolution(){
     let solvedDoku = solve(compareDoku);
 
     let arraysEqual = true;
+    let incorrectInputs = [];
     for (let i=0; i<9; i++){
         for (let j=0; j<9; j++){
             if (solvedDoku[i][j] !== userSolution[i][j]){
+                incorrectInputs.push([i,j]);
                 arraysEqual = false;
             }
         }
@@ -86,9 +101,68 @@ function compareSolution(){
     }
 
     if (arraysEqual){
-        alert('Correctly Solved');
+        // alert('Correctly Solved');
+        appendCorrect()
     } else {
-        alert('Solution Incorrect!');
+        // alert('Solution Incorrect!');
+        appendIncorrect(incorrectInputs);
     }
 }
 
+function appendCorrect(){
+    let appendHere = document.getElementById('solutionSection');
+    appendHere.innerHTML = "";
+    let congratsPara = document.createElement('p');
+    let toHide = document.getElementById('submitSection');
+    toHide.style.display = "none";
+    congratsPara.textContent = 'Congratulations! Your solution was correct!'
+    appendHere.append(congratsPara);
+    let userInputsNodeList = document.querySelectorAll('input[id]:not([id=""]');
+    let inputsArray = Array.from(userInputsNodeList);
+    for (let i=0; i<inputsArray.length; i++){
+        inputsArray[i].classList.add('correct');
+    }
+}
+
+function appendIncorrect(incorrectArray){
+    let appendHere = document.getElementById('solutionSection');
+    appendHere.innerHTML = "";
+    let failPara = document.createElement('p');
+    let checkWrongButton = document.createElement('button');
+    checkWrongButton.textContent = 'Where did I go wrong?';
+    checkWrongButton.id = "wrongBtn";
+    checkWrongButton.addEventListener('click', () => {
+        revealIncorrectInput(incorrectArray);
+    });
+    failPara.textContent = 'Unfortunately your solution was incorrect, click the button below to see where you went wrong or try again.'
+    let lineBreak = document.createElement('br');
+    appendHere.append(failPara);
+    appendHere.append(lineBreak);
+    appendHere.append(checkWrongButton);
+}
+
+function revealIncorrectInput(incorrectArray){
+    for (let i=0; i<incorrectArray.length; i++){
+        let incorrectInputBox = document.getElementById(`${incorrectArray[i][0]}${incorrectArray[i][1]}`);
+        incorrectInputBox.classList.add('incorrect');
+    };
+    let wrongButton = document.getElementById("wrongBtn");
+    wrongButton.textContent = 'Hide where I went wrong';
+    wrongButton.removeEventListener('click', revealIncorrectInput);
+    wrongButton.addEventListener('click', () => {
+        hideIncorrectInput(incorrectArray)
+    });
+}
+
+function hideIncorrectInput(incorrectArray){
+    for (let i=0; i<incorrectArray.length; i++){
+        let incorrectInputBox = document.getElementById(`${incorrectArray[i][0]}${incorrectArray[i][1]}`);
+        incorrectInputBox.classList.remove('incorrect');
+    };
+    let wrongButton = document.getElementById("wrongBtn");
+    wrongButton.textContent = 'Show where I went wrong';
+    wrongButton.removeEventListener('click', hideIncorrectInput);
+    wrongButton.addEventListener('click', () => {
+        revealIncorrectInput(incorrectArray)
+    });
+}
